c0802b3f595bc4eb7532ab5b58f43d697dc9ba09
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-07-23 23:25:58 +0200

    setup cmake

---
+++ examples/cli/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <microservice-essentials/context.h>
+
+int main()
+{
+    microservice_essentials::Context ctx;
+
+    std::cout << "cli service" << std::endl;
+    return 0;
+}
