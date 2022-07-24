fa624d6b8b70fa87b71e3b576dfeb003fa544ca4
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-07-24 22:52:06 +0200

    added test_package

---
+++ test_package/main.cpp
@@ -0,0 +1,9 @@
+#include <microservice-essentials/context.h>
+#include <iostream>
+
+int main()
+{
+    microservice_essentials::Context ctx;
+    std::cout << "context created" << std::endl;
+    return 0;    
+}
