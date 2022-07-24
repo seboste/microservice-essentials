1fbf565d6f3f041a71e18f9e3651b935c3f3bd71
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-07-24 23:08:51 +0200

    build examples and tests optionally

--- conanfile.py
+++ conanfile.py
@@ -6,17 +6,30 @@ class MicroserviceEssentials(ConanFile):
     license = "MIT"
     author = "Sebastian Steger (Sebastian.Steger@gmail.com)"
     description = """microservice-essentials is a portable, independent C++ library that takes care of typical recurring concerns that occur in microservice development."""
-    settings = "os", "compiler", "build_type", "arch"
-    requires = "catch2/2.13.3"
+    settings = "os", "compiler", "build_type", "arch"    
     generators = "cmake_find_package_multi"
     scm = {
         "type": "git",        
         "url": "https://github.com/seboste/microservice-essentials.git",
         "revision": "auto"
     }
+    options = {
+        "build_tests": [True, False],
+        "build_examples": [True, False]        
+    }
+    default_options = {
+        "build_tests": False,
+        "build_examples": False
+    }
+
+    def requirements(self):
+        if self.options.build_tests:
+            self.requires("catch2/2.13.3")
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions['BUILD_TESTS'] = self.options.build_tests
+        cmake.definitions['BUILD_EXAMPLES'] = self.options.build_examples
         cmake.configure()
         cmake.build()
 
