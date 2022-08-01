7952d9dc4b53aa906244e860c9bb8703f65338fe
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-08-01 22:32:51 +0200

    instantiate client + repo in main

--- examples/star-wars-starships/app/main.cpp
+++ examples/star-wars-starships/app/main.cpp
@@ -1,8 +1,13 @@
 #include <core/core.h>
+#include <adapters/in-memory-status-repo/in-memory-status-repo.h>
+#include <adapters/http-starwars-client/http-starwars-client.h>
 
 int main()
 {
-    Core core();
+    HttpStarWarsClient client("swapi.dev/api/");
+    InMemoryStatusRepo repo;
+
+    Core core(client, repo);
 
 
     return 0;
