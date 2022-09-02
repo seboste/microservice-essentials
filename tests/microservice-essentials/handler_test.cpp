c1022784cf88dd15536b17bed2a9b7d1709644ba
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-09-02 18:29:02 +0200

    test for the handler

--- tests/microservice-essentials/handler_test.cpp
+++ tests/microservice-essentials/handler_test.cpp
@@ -1,9 +1,42 @@
 #include <catch2/catch_test_macros.hpp>
-#include <microservice-essentials/context.h>
+#include <microservice-essentials/handler.h>
+#include <microservice-essentials/cross-cutting-concerns/graceful-shutdown.h>
 
 
-TEST_CASE( "Handler", "[handler]" ) 
+SCENARIO( "Handler", "[handler][graceful-shutdown]" ) 
 {
-    mse::Context ctx;
-    REQUIRE( 1 == 1 );
+    GIVEN("a handler")
+    {        
+        class TestHandler : public mse::Handler
+        {
+            public:
+                bool stopCalled = false;
+
+                TestHandler() : mse::Handler("test-handler") {}
+
+                virtual void Handle() override {}
+                virtual void Stop() override { stopCalled = true; }
+        };
+
+        {
+            TestHandler handler;
+
+            WHEN("Shutdown is requested")
+            {
+                mse::GracefulShutdown::GetInstance().Shutdown();
+                THEN("Handler::Stop is being called")
+                {
+                    REQUIRE(handler.stopCalled == true);
+                }
+            }
+        }
+
+        WHEN("The handler is deleted")
+        {
+            THEN("creating a new handler is possible")
+            {
+                REQUIRE_NOTHROW(TestHandler());
+            }
+        }
+    }    
 }
