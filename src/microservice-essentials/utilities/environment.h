3672eb55f7e6274b7b7ebd6c9019b3640fb35956
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-09-12 14:52:15 +0200

    move environment template implementation to txx file

--- src/microservice-essentials/utilities/environment.h
+++ src/microservice-essentials/utilities/environment.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <cstdlib>
-#include <exception>
 #include <optional>
-#include <sstream>
 #include <string>
-
 
 namespace mse
 {
@@ -15,57 +11,6 @@ template<typename T> T getenv(const std::string& env_name);
 template<typename T> T getenv_or(const std::string& env_name, T&& default_value);
 std::string getenv_or(const std::string& env_name, const char* default_value);
 
-template<>
-inline std::optional<std::string> getenv_optional(const std::string& env_name)
-{
-    char* env = std::getenv(env_name.c_str());
-    if(env == nullptr)
-    {
-        return std::nullopt;
-    }
-    return std::optional<std::string>(std::string(env));
 }
 
-template<typename T> 
-inline std::optional<T> getenv_optional(const std::string& env_name)
-{
-    std::optional<std::string> env = mse::getenv_optional<std::string>(env_name);
-    if(!env.has_value())
-    {
-        return std::nullopt;
-    }
-        
-    std::stringstream stream(env.value());
-    T value;
-    if(!(stream >> value))
-    {
-        throw std::invalid_argument(std::string("the value '") + env.value() + " of the environment variable '" + env_name + "' cannot be converted.");
-    }
-    return std::optional<T>(value);
-}
-
-template<typename T> 
-inline T getenv(const std::string& env_name)
-{
-    std::optional<T> opt_val = mse::getenv_optional<T>(env_name);
-    if(!opt_val.has_value())
-    {
-        throw std::invalid_argument(std::string("environment variable '") + env_name + "' is not set.");
-    }
-    return opt_val.value();
-}
-
-template<typename T> 
-inline T getenv_or(const std::string& env_name, T&& default_value)
-{
-    std::optional<T> opt_val = mse::getenv_optional<T>(env_name);
-    if(!opt_val.has_value())
-    {
-        return std::move(default_value);
-    }
-    return opt_val.value();
-}
-
-
-
-}
+#include "environment.txx"
