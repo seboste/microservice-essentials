313c70271439739af0973ffcdfd67b1f9ac47d68
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-10-04 19:50:12 +0200

    Request Processor Declaration

--- src/microservice-essentials/request/request-processor.h
+++ src/microservice-essentials/request/request-processor.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <microservice-essentials/request/request-decorator.h>
+#include <memory>
+#include <vector>
 
 namespace mse
 {
 
 class RequestProcessor
 {
-    public:
-        //With();
+public:
+    RequestProcessor& With(std::unique_ptr<RequestDecorator>&& decorator);
 
+    Status Process(RequestDecorator::Func func);
+private:
+    std::vector<std::unique_ptr<RequestDecorator>> _decorators;
 };
 
 }
