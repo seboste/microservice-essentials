ee94f154797f6cdc9298cfb1a125b3da137eaf70
Steger, Sebastian <Sebastian.Steger@gmail.com>
2022-10-25 21:09:42 +0200

    exception handling request hook

---
+++ src/microservice-essentials/cross-cutting-concerns/exception-handling-request-hook.cpp
@@ -0,0 +1,47 @@
+#include "exception-handling-request-hook.h"
+
+using namespace mse;
+using namespace mse::ExceptionHandling;
+
+const std::vector<ExceptionHandlingRequestHook::ExceptionToStatusMapping> ExceptionHandlingRequestHook::_default_exception_to_status_mappings
+{ 
+    { Is<ExceptionOfType<std::invalid_argument>>()  , Status{StatusCode::invalid_argument,  "invalid argument exception"} },
+    { Is<ExceptionOfType<std::out_of_range>>()      , Status{StatusCode::out_of_range,      "out of range argument exception"} },
+    { Is<AnyException>()                            , Status{StatusCode::internal,          "unknown exception"} }  
+};
+
+ExceptionHandlingRequestHook::Parameters::Parameters(const std::vector<ExceptionToStatusMapping>& mappings)
+    : exception_to_status_mapping(mappings)
+{
+}
+
+ExceptionHandlingRequestHook::ExceptionHandlingRequestHook(const Parameters&)
+    : RequestHook("exception handling")
+    , _exception_to_status_mappings()
+{
+}
+
+ExceptionHandlingRequestHook::~ExceptionHandlingRequestHook()
+{
+}
+
+Status ExceptionHandlingRequestHook::Process(Func func, Context& context)
+{
+    try
+    {
+        return func(context);
+    }
+    catch(...)
+    {
+        //check all exception types that shall be handled
+        for(const ExceptionToStatusMapping& exception_to_status : _exception_to_status_mappings)
+        {
+            if(exception_to_status.first->Test(std::current_exception()))
+            {
+                return exception_to_status.second;
+            }
+        }
+        //rethrow 
+        throw;
+    }
+}
---
+++ src/microservice-essentials/cross-cutting-concerns/exception-handling-request-hook.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <microservice-essentials/request/request-hook.h>
+#include <microservice-essentials/request/request-hook-factory.h>
+#include <microservice-essentials/status.h>
+#include <exception>
+#include <vector>
+
+namespace mse
+{
+
+namespace ExceptionHandling
+{
+    class ExceptionTypePredicate
+    {
+    public:
+        virtual ~ExceptionTypePredicate() = default;
+        virtual bool Test(const std::exception_ptr& exception) const = 0;
+    };    
+
+    template<typename ExceptionType>
+    class ExceptionOfType : public ExceptionTypePredicate
+    {
+        public:
+            bool Test(const std::exception_ptr& exception) const override
+            {
+                try
+                {
+                    std::rethrow_exception(exception);
+                }
+                catch(const ExceptionType&)
+                {
+                    return true;
+                }
+                catch(...)
+                {
+                    return false;
+                }
+            }
+    };
+
+    class AnyException : public ExceptionTypePredicate
+    {
+        public:
+            bool Test(const std::exception_ptr&) const override { return true;}
+    };
+
+    template<typename Predicate>
+    std::shared_ptr<ExceptionTypePredicate> Is() { return std::make_shared<Predicate>(); }
+
+}
+
+class ExceptionHandlingRequestHook : public mse::RequestHook
+{
+public:
+
+    typedef std::pair<std::shared_ptr<ExceptionHandling::ExceptionTypePredicate>, mse::Status> ExceptionToStatusMapping;
+    
+    struct Parameters
+    {
+        Parameters(const std::vector<ExceptionToStatusMapping>& mappings = ExceptionHandlingRequestHook::_default_exception_to_status_mappings);
+        std::vector<ExceptionToStatusMapping> exception_to_status_mapping;        
+        AutoRequestHookParameterRegistration<ExceptionHandlingRequestHook::Parameters, ExceptionHandlingRequestHook> auto_registration;
+    };
+
+    ExceptionHandlingRequestHook(const Parameters& parameters);
+    virtual ~ExceptionHandlingRequestHook();    
+
+    virtual Status Process(Func func, Context& context) override;
+
+private:
+    std::vector<ExceptionToStatusMapping> _exception_to_status_mappings;    
+    static const std::vector<ExceptionToStatusMapping> _default_exception_to_status_mappings;
+};
+
+}
+
+
