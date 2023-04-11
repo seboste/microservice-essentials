50374d6b3085535df92b8d5c8e91fed2d04401f7
Steger, Sebastian <Sebastian.Steger@gmail.com>
2023-04-11 17:13:59 +0200

    documentation

--- src/microservice-essentials/reliability/retry-request-hook.h
+++ src/microservice-essentials/reliability/retry-request-hook.h
@@ -10,15 +10,22 @@
 namespace mse
 {
 
+//abstract backoff strategy for RetryRequestHook
 class RetryBackoffStrategy
 {
 public:
     virtual ~RetryBackoffStrategy() = default;
 
     using Duration = std::chrono::duration<double, std::milli>;
+
+    //returns the duration until the next retry shall be attempted based on the current retry_count (will be called with 
+    //a value of 1 for the first time) and the duration the request has already taken. Should return 0ms if a retry shall be
+    //performed instantly.
+    //In case no further attemps shall be taken, nullopt is returned.
     virtual std::optional<Duration> GetDurationUntilNextRetry(uint32_t retry_count, Duration total_request_duration) const = 0;
 };
 
+//retries in regular intervals (e.g. after 10, 20, 30 ms for _max_retry_count==3 and _retry_interval==10ms)
 class LinearRetryBackoff : public RetryBackoffStrategy
 {
 public:
@@ -29,6 +36,7 @@ private:
     Duration _retry_interval;
 };
 
+//retries in exponential increasing intervals (e.g. after 10, 10+20, 10+20+40 ms for _max_retry_count==3, _first_retry_interval==10ms, and _base==2)
 class ExponentialRetryBackoff : public RetryBackoffStrategy
 {
 public:
@@ -52,6 +60,17 @@ class BackoffGaussianJitterDecorator : public RetryBackoffStrategy
         Duration _sigma;
 };
 
+/**
+ * Request hook for outgoing requests that retries requests that return certain error codes
+ * after durations defined by a backoff strategy (e.g. ExponentialRetryBackoff)
+ * 
+ * To avoid a retry storm (i.e. many clients retrying at the very same time), it is recommended to randomly
+ * apply jitter to the retry intervalls (see BackoffGaussianJitterDecorator).
+ * 
+ * In case retries shall be performed in case of exceptions, consider using the ExceptionHandlingRequestHook to
+ * convert an exception to a status code that results in retries.
+ * 
+*/
 class RetryRequestHook : public mse::RequestHook
 {
 public:
@@ -60,7 +79,10 @@ public:
 
     struct Parameters
     {
-        Parameters(std::shared_ptr<RetryBackoffStrategy> strategy, const std::set<mse::StatusCode>& retry_ec = { mse::StatusCode::unavailable, mse::StatusCode::internal });
+        Parameters(
+            std::shared_ptr<RetryBackoffStrategy> strategy,
+            const std::set<mse::StatusCode>& retry_ec = { mse::StatusCode::unavailable, mse::StatusCode::resource_exhausted, mse::StatusCode::internal }
+            );
         
         std::shared_ptr<RetryBackoffStrategy> backoff_strategy;
         std::set<mse::StatusCode> retry_error_codes;
@@ -80,6 +102,5 @@ private:
     uint32_t _retry_counter = 0;
     Func _func;
 };
-
 
 }
