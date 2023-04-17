674f47fb7a2a1a5a7ecb1b1bbe4b3a51257cd63d
Steger, Sebastian <Sebastian.Steger@gmail.com>
2023-04-17 13:05:03 +0200

    adapt example

--- examples/star-wars-starships/adapters/http-starwars-client/http-starwars-client.h
+++ examples/star-wars-starships/adapters/http-starwars-client/http-starwars-client.h
@@ -2,18 +2,24 @@
 
 #include <ports/starwars-client.h>
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace httplib { class Client; }
 
 class HttpStarWarsClient : public StarWarsClient
 {
 public:
-    HttpStarWarsClient(const std::string& url = "https://swapi.dev");
+    HttpStarWarsClient(
+        const std::string& url,                                 //where to reach find the star wars endoints? (e.g. https://swapi.dev)
+        const std::vector<std::string>& headers_to_propagate    //which headers shall be propagated from this service to the star wars service? (e.g. tracing headers or authorization headers)        
+    );
     virtual ~HttpStarWarsClient();
 
     virtual std::vector<StarshipProperties> ListStarShipProperties() const override;
     virtual std::optional<StarshipProperties> GetStarShipProperties(const std::string& starshipId) const override;
 
 private:    
-    std::unique_ptr<httplib::Client> _cli;    
+    std::unique_ptr<httplib::Client> _cli;
+    std::vector<std::string> _headers_to_propagate;
 };
