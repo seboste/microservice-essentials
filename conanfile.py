7e8e1e0f8183e96337069a4c540ac866b4565f81
Sebastian Steger <sebastian@sebastian-steger.com>
2023-05-30 22:53:37 +0200

    set version to 0.1.0

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake
 class MicroserviceEssentials(ConanFile):
     name = "microservice-essentials"
-    version = "0.0.1"
+    version = "0.1.0"
     url = "https://github.com/seboste/microservice-essentials"
     license = "MIT"
     author = "Sebastian Steger (Sebastian.Steger@gmail.com)"
